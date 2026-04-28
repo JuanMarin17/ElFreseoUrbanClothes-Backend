4245bb5e9b12b26e60e22c3a93776494d02d569e
juan <juancamilou07@gmail.com>
2026-04-28 08:07:54 -0500

    fix: se corrigio el nombre del modulo de preferencias y se eliminaron carpetas inexistentes

--- apiProduct/src/main/java/com/api/product/repository/BrandRepository.java
+++
@@ -1,9 +0,0 @@
-package com.api.product.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.api.product.entity.Brand;
-
-import java.util.UUID;
-
-public interface BrandRepository extends JpaRepository<Brand, UUID> {
-}
