2a4d2e90581c48670f561b780fcfe595eca45315
juan <juancamilou07@gmail.com>
2026-05-05 06:36:10 -0500

    fix: actualización del modulo producto con las validaciones, y metodos de busqueda

--- Product/src/main/java/com/api/product/repository/ProductRepository.java
+++ Product/src/main/java/com/api/product/repository/ProductRepository.java
@@ -1,9 +1,26 @@
 package com.api.product.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.api.product.entity.Product;
 
-public interface ProductRepository extends JpaRepository <Product, Long> {
+import java.time.OffsetDateTime;
+import java.util.List;
+import java.util.UUID;
+
+public interface ProductRepository extends JpaRepository<Product, UUID> {
+
+    boolean existsByNameIgnoreCase(String name);
+
+    Page<Product> findByActiveTrue(Pageable pageable);
+
+    List<Product> findByCreatedAtAfter(OffsetDateTime date);
+
+    List<Product> findByActiveTrueAndCreatedAtAfter(OffsetDateTime date);
+    
+    List<Product> findByActiveTrue();
+
 
 }
