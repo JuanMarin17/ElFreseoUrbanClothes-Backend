6cf29ad4bb18a50fddc61c7baab1c7b700a02b33
juan <juancamilou07@gmail.com>
2026-05-20 06:18:17 -0500

    feat: nuevos metodos updateuser y findbyid

--- Users/src/main/java/com/api/Users/controller/UserController.java
+++ Users/src/main/java/com/api/Users/controller/UserController.java
@@ -7,17 +7,16 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-
-import jakarta.validation.Valid;
 
 import com.api.Users.dto.MessageResponseDTO;
 import com.api.Users.dto.UserDTO;
 import com.api.Users.service.UserService;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -39,8 +38,20 @@ public class UserController {
     }
 
     @GetMapping("/me")
-    public ResponseEntity<UserDTO> myProfile(){
+    public ResponseEntity<UserDTO> myProfile() {
         UserDTO response = userService.myProfile();
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+    
+    @PutMapping("/update/{id}")
+    public ResponseEntity<MessageResponseDTO> updateUser(@PathVariable("id") UUID id, @RequestBody UserDTO userDTO) {
+
+        MessageResponseDTO response = userService.updateUser(id, userDTO);
+        return ResponseEntity.status(response.getStatus()).body(response);
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDTO> getUserById(@PathVariable("id") UUID id) {
+        UserDTO response = userService.getUserById(id);
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 }
