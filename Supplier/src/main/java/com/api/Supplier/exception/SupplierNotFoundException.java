ebddef3447ff5741cc0053a649846dc144fb307f
juan <juancamilou07@gmail.com>
2026-05-20 19:33:46 -0500

    fix: se arregla problemas de dependencias

--- Supplier/src/main/java/com/api/Supplier/exception/SupplierNotFoundException.java
+++ Supplier/src/main/java/com/api/Supplier/exception/SupplierNotFoundException.java
@@ -6,4 +6,8 @@ public class SupplierNotFoundException extends RuntimeException {
     public SupplierNotFoundException(UUID supplierId) {
         super("Supplier no encontrado con id: " + supplierId);
     }
+
+    public SupplierNotFoundException(String message) {
+        super(message);
+    }
 }
