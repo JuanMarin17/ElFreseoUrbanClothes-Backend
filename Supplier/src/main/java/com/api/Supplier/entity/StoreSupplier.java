b484be1d71f34763706cd3f7682fe3b39a96961c
juan <juancamilou07@gmail.com>
2026-05-20 19:13:59 -0500

    feat and fix: modulo de proveedor,se arregla el modulo de support v1.1 y rutas gateway

--- Supplier/src/main/java/com/api/Supplier/entity/StoreSupplier.java
+++ Supplier/src/main/java/com/api/Supplier/entity/StoreSupplier.java
@@ -1,45 +1,34 @@
 package com.api.Supplier.entity;
 
-import jakarta.persistence.*;
-import lombok.*;
-import org.hibernate.annotations.CreationTimestamp;
-
 import java.io.Serializable;
 import java.time.OffsetDateTime;
 import java.util.UUID;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
 @Entity
 @Table(name = "store_supplier")
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
-@IdClass(StoreSupplier.StoreSupplierKey.class)
 public class StoreSupplier {
 
-    @Id
-    @Column(name = "store_id", nullable = false)
-    private UUID storeId;
+    @EmbeddedId
+    private StoreSuppliedId id;
 
-    @Id
-    @Column(name = "supplier_id", nullable = false)
-    private UUID supplierId;
+    @Column(name = "created_at")
+    private OffsetDateTime createdAt = OffsetDateTime.now();
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "supplier_id", insertable = false, updatable = false)
-    private Supplier supplier;
-
-    @CreationTimestamp
-    @Column(name = "created_at", updatable = false)
-    private OffsetDateTime createdAt;
-
-    @Getter
-    @Setter
-    @NoArgsConstructor
-    @AllArgsConstructor
-    @EqualsAndHashCode
-    public static class StoreSupplierKey implements Serializable {
+    @Data
+    @Embeddable
+    public static class StoreSuppliedId implements Serializable {
+        @Column(name = "store_id")
         private UUID storeId;
+
+        @Column(name = "supplier_id")
         private UUID supplierId;
     }
 }
