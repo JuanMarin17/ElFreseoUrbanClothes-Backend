c629859ad743c2a772443e0ed3faac43cece8a5a
juan <juancamilou07@gmail.com>
2026-05-21 21:31:35 -0500

    fix: arreglo de path

---
+++ Returns/src/main/java/com/api/Returns/ReturnsApplication.java
@@ -0,0 +1,13 @@
+package com.api.Returns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReturnsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReturnsApplication.class, args);
+	}
+
+}
