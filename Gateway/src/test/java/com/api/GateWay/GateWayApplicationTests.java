c6f81d2acaf73164056943ac7f83515cf8567f36
JuanMarin <juandavidmr77@gmail.com>
2026-06-13 15:27:53 -0500

    errores en gateway

--- Gateway/src/test/java/com/api/GateWay/GateWayApplicationTests.java
+++ Gateway/src/test/java/com/api/GateWay/GateWayApplicationTests.java
@@ -3,7 +3,9 @@ package com.api.GateWay;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import com.api.gateway.GateWayApplication;
+
+@SpringBootTest(classes = GateWayApplication.class)
 class GateWayApplicationTests {
 
 	@Test
