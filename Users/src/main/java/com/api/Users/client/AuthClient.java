d5435f2885b218f46cfb88963c0893257ae41481
JuanMarin <juandavidmr77@gmail.com>
2026-06-13 10:07:49 -0500

    fix: habian unos errores en los clients del modulo Users y al momento de reconstruirlo en el servidor daba error porque no estaban definidos de manera correcta

--- Users/src/main/java/com/api/Users/client/AuthClient.java
+++ Users/src/main/java/com/api/Users/client/AuthClient.java
@@ -2,6 +2,7 @@ package com.api.Users.client;
 
 import java.util.UUID;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 
@@ -9,8 +10,8 @@ import org.springframework.web.reactive.function.client.WebClient;
 public class AuthClient {
     private final WebClient authWebClient;
 
-    public AuthClient(WebClient authClient){
-        this.authWebClient = authClient;
+    public AuthClient(@Qualifier("authWebClient") WebClient authWebClient){
+        this.authWebClient = authWebClient;
     }
 
     public String getEmail(UUID id){
